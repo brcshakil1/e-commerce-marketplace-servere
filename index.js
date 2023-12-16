49175291eca00cc344f1f848b727ddf651644903
brcshakil1 <brcshakil72@gmail.com>
2023-12-16 10:55:38 +0600

    seller catalog api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
@@ -29,24 +30,34 @@ async function run() {
     await client.connect();
 
     // all collections
-    const usersCollections = client.db("unityDB").collection("users");
+    // users
+    const usersCollection = client.db("unityDB").collection("users");
+    // catalog's collection
+    const catalogsCollection = client.db("unityDB").collection("catalogs");
+
+    // auth related api
+    // login
+    app.post("/api/auth/login", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "1h" });
+      console.log("token is -----> ", token);
+      res.send({ token });
+    });
 
     // users
-    app.put("/api/auth/register/:email", async (req, res) => {
-      const email = req.params.email;
+    app.put("/api/auth/register", async (req, res) => {
       const user = req.body;
-      console.log(email, user);
-      const query = { email: email };
+      const query = { email: user.email };
       const options = { upsert: true };
 
       //   checking is user already exist
-      const isExist = await usersCollections.findOne(query);
+      const isExist = await usersCollection.findOne(query);
 
       // If the user is already created an account then return his previous data
-      if (isExist) res.send(isExist);
+      if (isExist) return res.send(isExist);
 
       // if user is new then create a new data
-      const result = await usersCollections.updateOne(
+      const result = await usersCollection.updateOne(
         query,
         {
           $set: { ...user, timeStamp: Date.now() },
@@ -54,6 +65,51 @@ async function run() {
         options
       );
       res.send(result);
+    });
+
+    // APIs for buyer
+    app.get("/api/buyer/list-of-sellers", async (req, res) => {
+      const query = { typeOfUser: "seller" };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // app.get("/api/buyer/seller-catalog/:seller_id", async(req, res)=)
+
+    // APIs for sellers
+    app.post("/api/seller/create-catalog", async (req, res) => {
+      const sellerId = req.body.sellerId;
+      const sellerCatalog = await catalogsCollection.findOne({ sellerId });
+      if (!sellerCatalog) {
+        const newCatalog = {
+          sellerId,
+          catalog: req.body.catalog,
+          items: req.body.items || [],
+        };
+        const result = await catalogsCollection.insertOne(newCatalog);
+        res
+          .status(201)
+          .send({ message: "Successfully created catalog", data: result });
+      } else {
+        const sellerCatalog = await catalogsCollection.findOne({ sellerId });
+        const existingItems = sellerCatalog.items.map((item) => item.name);
+        console.log(existingItems);
+        const newItems = req.body?.items?.filter(
+          (item) => !existingItems?.includes(item.name)
+        );
+
+        if (newItems.length > 0) {
+          const updateCatalog = {
+            $push: { items: { $each: newItems || [] } },
+          };
+          await catalogsCollection.updateOne({ sellerId }, updateCatalog);
+          res
+            .status(201)
+            .send({ message: "Successfully inserted new items on catalog" });
+        } else {
+          res.status(400).send({ message: "All items already exist" });
+        }
+      }
     });
 
     // Send a ping to confirm a successful connection
